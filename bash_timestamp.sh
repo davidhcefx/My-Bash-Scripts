ec25944f3c5d853040ae0b81fdbf748470b62f9a
davidhcefx <davidhu0903ex3@gmail.com>
2019-10-10 02:19:29 +0800

    Update bash_timestamp.sh

--- bash_timestamp.sh
+++ bash_timestamp.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 #
-# Please set up to run this script upon user login.
+# Please set up to run this script upon user login. You may put this file somewhere else other than ~/bin.
 
 printf "\n[-- System Login: $USER@`uname -n`, $(date +%-m/%-d:%H) --]\n\n" >> ~/.bash_history
