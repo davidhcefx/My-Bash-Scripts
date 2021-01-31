bc712490022d3ec4a759197a890eec03e58da65d
davidhcefx <davidhu0903ex3@gmail.com>
2021-02-01 04:08:03 +0800

    Linted, changed parsing framework and enhancements
    - Bugfixes:
        - corrected path after `#!`
        - disable regex for grep
    - Enhancements:
        - pylinted (6.7 -> 8.3 out of 10)
        - changed to use `argparse` framework
    - Features:
        - follow symlink when listing files by default
        - changable default-extension

--- Search Keyword in Files/findinfiles.py
+++ Search Keyword in Files/findinfiles.py
@@ -1,70 +1,65 @@
-#! /bin/python3
+#! /usr/bin/python3
 #
 # Search for keywords in files reachable from current directory.
 # Written by davidhcefx, 2020.7.19. Originally in Bash.
 
-from sys import argv
-from os.path import basename
-import readline
+import readline  # for better input handling
 from subprocess import run, PIPE
+from argparse import ArgumentParser, Namespace
 
+DEFAULT_EXT = 'c|cpp'
 
-VERBOSE = False
-INSENSITIVE = False
-
-def help():
-    print(f'Syntax: {basename(argv[0])} (-i|-v|-h)')
-    print('\t-i:\tCase insensitive search.')
-    print('\t-v:\tVerbose.')
-    print('\t-h:\tHelp.')
 
 def green(text):
     return "\33[32m" + text + "\33[0m"
 
-def main():
+def main(args: Namespace):
     # configure extension
-    ext = input('Extensions: [c|cpp] ')
+    ext = input('Extensions: [{}] '.format(DEFAULT_EXT))
 
     if ext == '':
-        ext = 'c|cpp'
+        ext = DEFAULT_EXT
     elif ext.startswith('[') and ext.endswith(']'):
         ext.strip('[]')
     elif ext.startswith('(') and ext.endswith(')'):
         ext.strip('()')
 
-    r = run(['find', '.', '-type', 'f', '-regex',
-            '.*/.*\\.\\(' + ext.replace('|', '\\|') + '\\)'], stdout=PIPE)
-    r.check_returncode()
+    res = run(['find', (args.symlink and '-L' or '-P'), '.', '-type', 'f', '-regex',
+            '.*/.*\\.\\(' + ext.replace('|', '\\|') + '\\)'], stdout=PIPE, check=True)
 
-    if len(r.stdout) == 0:
-        print(f"It seems that no file match extension '{ext}'")
-        exit(1)
+    if len(res.stdout) == 0:
+        raise SystemExit("It seems that no file match extension '{}'".format(ext))
 
-    # configure search string (format: bytes)
-    search_str = input('Search string: ').encode('utf8')
-    if INSENSITIVE:
+    # configure search string
+    search_str = input('Search string: ').encode('utf8')  # type: bytes
+    if args.icase:
         search_str = search_str.upper()
 
-    print("Searching for '" + search_str.decode('utf8') + "' ...")
+    print("Searching for '{}' ...".format(search_str.decode('utf8')))
 
-    for name in r.stdout.decode('utf8').strip('\n').split('\n'):
+    for name in res.stdout.decode('utf8').strip('\n').split('\n'):
         name_shown = False
         for line in open(name, 'rb').read().split(b'\n'):
-            if search_str in ((INSENSITIVE and line.upper()) or line):
-                if not name_shown or VERBOSE:
+            if search_str in (args.icase and line.upper() or line):
+                # found a match
+                if not name_shown or args.verbose:
                     print(green(name))
                     name_shown = True
-                run(['grep', '--color=always', search_str], input=line)
+
+                run(['grep', '--color=always', '-F', search_str], input=line, check=True)
 
 
 if __name__ == '__main__':
-    if len(argv) > 1:
-        for arg in argv[1:]:
-            if arg == '-i':
-                INSENSITIVE = True
-            elif arg == '-v':
-                VERBOSE = True
-            elif arg == '-h':
-                help()
-                exit(0)
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('-i', dest='icase', action='store_true', help='case insensitive search')
+    parser.add_argument('-v', dest='verbose', action='store_true', help='verbose')
+    parser.add_argument('-nL', dest='symlink', action='store_false',
+                        help='do not follow symbolic link when listing files')
+    main(args=parser.parse_args())
+
+
+
+## Changelog
+# - 1/31:
+#   - corrected path after #!, pylinted (6.7 -> 8.3), changed to argparse framework
+#   - follow symlink by default, changable default-extension, disable regex for grep
