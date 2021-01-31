42b282c04aebedc78ed9a36b76ce3e815050e42d
davidhcefx <davidhu0903ex3@gmail.com>
2021-02-01 06:12:26 +0800

    Bugfix for case-insensitive and better readability
    - Bugfix:
        - can grep case-insensitive patterns through `grep -P` plus escapings
    - Readability:
        - prefer (X if C else Y) over (C and X or Y)
        - reduce encode/decode usages

--- Search Keyword in Files/findinfiles.py
+++ Search Keyword in Files/findinfiles.py
@@ -4,6 +4,7 @@
 # Written by davidhcefx, 2020.7.19. Originally in Bash.
 
 import readline  # for better input handling
+import string
 from subprocess import run, PIPE
 from argparse import ArgumentParser, Namespace
 
@@ -12,6 +13,9 @@ DEFAULT_EXT = 'c|cpp'
 
 def green(text):
     return "\33[32m" + text + "\33[0m"
+
+def escape_regex(text):
+    return text.translate({ord(p): '\\' + p for p in string.punctuation})
 
 def main(args: Namespace):
     # configure extension
@@ -24,29 +28,29 @@ def main(args: Namespace):
     elif ext.startswith('(') and ext.endswith(')'):
         ext.strip('()')
 
-    res = run(['find', (args.symlink and '-L' or '-P'), '.', '-type', 'f', '-regex',
+    res = run(['find', ('-L' if args.symlink else '-P'), '.', '-type', 'f', '-regex',
             '.*/.*\\.\\(' + ext.replace('|', '\\|') + '\\)'], stdout=PIPE, check=True)
 
     if len(res.stdout) == 0:
         raise SystemExit("It seems that no file match extension '{}'".format(ext))
 
     # configure search string
-    search_str = input('Search string: ').encode('utf8')  # type: bytes
+    search_str = input('Search string: ')
     if args.icase:
         search_str = search_str.upper()
 
-    print("Searching for '{}' ...".format(search_str.decode('utf8')))
+    print("Searching for '{}' ...".format(search_str))
 
     for name in res.stdout.decode('utf8').strip('\n').split('\n'):
         name_shown = False
-        for line in open(name, 'rb').read().split(b'\n'):
-            if search_str in (args.icase and line.upper() or line):
-                # found a match
+        for line in open(name, 'rb').read().split(b'\n'):  # type: bytes
+            if search_str.encode('utf8') in (line.upper() if args.icase else line):
                 if not name_shown or args.verbose:
                     print(green(name))
                     name_shown = True
 
-                run(['grep', '--color=always', '-F', search_str], input=line, check=True)
+                pattern = ('(?i)' if args.icase else '') + escape_regex(search_str)
+                run(['grep', '--color=always', '-P', pattern], input=line, check=True)
 
 
 if __name__ == '__main__':
@@ -63,3 +67,6 @@ if __name__ == '__main__':
 # - 1/31:
 #   - corrected path after #!, pylinted (6.7 -> 8.3), changed to argparse framework
 #   - follow symlink by default, changable default-extension, disable regex for grep
+# - 2/1:
+#   - grep pattern case-insensitively, prefer (X if C else Y) over (C and X or Y)
+#   - reduce encode/decode usage
